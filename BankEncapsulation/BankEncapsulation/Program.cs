84308585c0f4f50d92e5e996a26d14bb2438b29a
calebtripp <calebtripp@outlook.com>
2022-09-24 18:48:18 -0400

    Bank Encapsulation Finished

--- BankEncapsulation/BankEncapsulation/Program.cs
+++ BankEncapsulation/BankEncapsulation/Program.cs
@@ -2,84 +2,106 @@
 using System.Transactions;
 
 BankAccount user1 = new BankAccount();
-//var choice = Console.ReadLine();
-//var deposit = user1.Deposit;
-//var withdrawal = user1.Withdrawal;
-//var balance = user1.GetBalance;
-int newBalance;
+
 Console.WriteLine("Please review the following options then enter your selection below\n");
 Console.WriteLine("Type the word 'deposit' then press enter to make a deposit.\n");
 Console.WriteLine("Type the word 'withdrawal' then press enter to make a withdrawal.\n");
 Console.WriteLine("Type the word 'balance' then press enter to view your current balance.\n");
-//Console.ReadLine();
-//while (Console.ReadLine() != "deposit" || Console.ReadLine() != "withdrawal" || Console.ReadLine() != "balance")
-//{
-//   // Console.WriteLine("We couldn't understand your selection, please try again");
-//}
-while (Console.ReadLine() == "deposit")
+
+var response = Console.ReadLine();
+
+while (response != "deposit" && response != "withdrawal" && response != "balance")
+{
+    Console.WriteLine();
+    Console.WriteLine("Please review the following options then enter your selection below\n");
+    Console.WriteLine("Type the word 'deposit' then press enter to make a deposit.\n");
+    Console.WriteLine("Type the word 'withdrawal' then press enter to make a withdrawal.\n");
+    Console.WriteLine("Type the word 'balance' then press enter to view your current balance.\n");
+
+    var response2 = Console.ReadLine();
+
+    while (response2 == "deposit")
+    {
+        Console.WriteLine("Great, you're making a deposit. Please enter the amount you would like to deposit then press enter");
+        bool couldParse = double.TryParse(Console.ReadLine(), out double dollarsCents);
+
+        while (!couldParse)
+        {
+            Console.WriteLine("Your input wasn't a number, please try again.");
+            Console.WriteLine("What amount would you like to Deposit?");
+            couldParse = double.TryParse(Console.ReadLine(), out dollarsCents);
+        }
+        user1.Deposit(dollarsCents);
+        Console.WriteLine($"Your deposit was successful. Your current balance is now:");
+        Console.WriteLine(user1.GetBalance());
+        break;
+    }
+
+    while (response2 == "balance")
+    {
+        Console.WriteLine("Your Balance is currently:");
+        Console.WriteLine(user1.GetBalance());
+        break;
+    }
+
+    while (response2 == "withdrawal")
+    {
+        Console.WriteLine("Great, you're making a withdrawal. Please enter the amount you would like to withdraw then press enter");
+        bool couldParse = double.TryParse(Console.ReadLine(), out double dollarsCents);
+
+        while (!couldParse)
+        {
+            Console.WriteLine("Your input wasn't a number, please try again.");
+            Console.WriteLine("What amount would you like to withdraw?");
+            couldParse = double.TryParse(Console.ReadLine(), out dollarsCents);
+        }
+        user1.Withdrawal(dollarsCents);
+        Console.WriteLine($"Your withdrawal was successful. Your current balance is now:");
+        Console.WriteLine(user1.GetBalance());
+        break;
+    }
+}
+
+
+while (response == "deposit")
 {
     Console.WriteLine("Great, you're making a deposit. Please enter the amount you would like to deposit then press enter");
-    //Console.WriteLine();
     bool couldParse = double.TryParse(Console.ReadLine(), out double dollarsCents);
 
     while (!couldParse)
     {
         Console.WriteLine("Your input wasn't a number, please try again.");
-
         Console.WriteLine("What amount would you like to Deposit?");
-
         couldParse = double.TryParse(Console.ReadLine(), out dollarsCents);
     }
     user1.Deposit(dollarsCents);
     Console.WriteLine($"Your deposit was successful. Your current balance is now:");
     Console.WriteLine(user1.GetBalance());
-    
     break;
 }
 
-//Console.WriteLine("Great, you're making a deposit. Please enter the amount you would like to deposit then press enter");
+while (response == "balance")
+{
+    Console.WriteLine("Your Balance is currently:");
+    Console.WriteLine(user1.GetBalance());
+    break;
+}
 
+while (response == "withdrawal")
+{
+    Console.WriteLine("Great, you're making a withdrawal. Please enter the amount you would like to withdraw then press enter");
+    bool couldParse = double.TryParse(Console.ReadLine(), out double dollarsCents);
 
-//Console.WriteLine("Great, you're making a withdrawal. Please enter the amount you would like to withdraw then press enter.");
-//toLower
+    while (!couldParse)
+    {
+        Console.WriteLine("Your input wasn't a number, please try again.");
+        Console.WriteLine("What amount would you like to withdraw?");
+        couldParse = double.TryParse(Console.ReadLine(), out dollarsCents);
+    }
+    user1.Withdrawal(dollarsCents);
+    Console.WriteLine($"Your withdrawal was successful. Your current balance is now:");
+    Console.WriteLine(user1.GetBalance());
+    break;
+}
 
-
-
-
-
-
-// if one of the strings, then do that method. otherwise, 
-
-//Console.WriteLine("How much money would you like to deposit today?");
-
-////
-
-////
-
-
-
-//Console.WriteLine("How much money would you like to Withdraw today?");
-//Console.ReadLine();
-
-//user1.Withdrawal(56);
-
-//Console.WriteLine("Your Balance is currently:");
-
-//Console.WriteLine(user1.GetBalance);
-    
-
-
-
-
-
-
-
-
-//        //BankAccount.Deposit(Convert.ToDouble(Console.ReadLine()));
-
-//        //Console.WriteLine(); BankAccount.balance + (Console.ReadLine());
-   
-        
-
-
-
+//I feel like there's a shorter way to have all the same redundancies for user input error?
